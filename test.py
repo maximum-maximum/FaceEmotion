0fbea69fd457b9e17db7727a58931cc12c2c684d
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-01-05 17:44:21 +0900

    typo，細かな修正

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ import os
 
 face_cascade_path = '/Users/makishima/opt/anaconda3/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
+
 box_list = []
 img_list = []
 
@@ -30,8 +31,8 @@ def get_img_data(f, maxsize=(600, 450), first=False):
 
 
 def get_face_position(img_path):
-  # print('get')
   img = cv2.imread(img_path)
+  print(img_path)
 #     plt.imshow(img, cmap='gray')
   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   # face_box = face_cascade.detectMultiScale(img_gray)
@@ -44,34 +45,29 @@ def get_face_position(img_path):
 
 
 for img_path in sys.argv:
-  if img_path == 'test6.py':
+  if img_path == 'test.py':
     continue
   box_list.append(img_path)
   get_face_position(img_path)
 
-print(box_list)
-
-filename = './img/test.jpg'  # 最初のファイル
-asci_image = './img/test2.jpg'
 
 for box in box_list:
   img_list.append(sg.Image(data=get_img_data(box, first=True)))
 
 
 # layout = [col_read_file, img_list]
-layout = [[sg.Text('数字を入力してください'), sg.InputText()],
-          [sg.Button('OK'), sg.Button('キャンセル')],
+layout = [[sg.Text('Please enter a number.'), sg.InputText()],
+          [sg.Button('OK'), sg.Button('Cancel')],
           img_list]
 
 window = sg.Window('Choose face', layout, return_keyboard_events=True, use_default_focus=False)
 
 # loop reading the user input and displaying image, filename
-
 while True:
   event, values = window.read()
-  if event == sg.WIN_CLOSED or event == 'キャンセル':
+  if event == sg.WIN_CLOSED or event == 'Cancel':
     break
   elif event == 'OK':
-    print('あなたが入力した値： ', values[0])  # emotions[int(values[0])]
+    print('What you entered：', values[0])  # emotions[int(values[0])]
 
 window.close()
