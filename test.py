fe7a0c3b42495aa38e7d00f1abf6e6514075f263
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-01-05 23:19:03 +0900

    input->cv2->複数表示

--- test.py
+++ test.py
@@ -12,15 +12,17 @@ import os
 face_cascade_path = '/Users/makishima/opt/anaconda3/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
-box_list = []
+path_list = []
 img_list = []
 
 
-def get_img_data(f, maxsize=(600, 450), first=False):
+def get_img_data(f, face_box, maxsize=(100, 120), first=False):
   """Generate image data using PIL
   """
-  print("open file:", f)
+  # print("open file:", f)
+  # print("face_box.shape:" + str(face_box.shape))
   img = Image.open(f)
+  img = img.crop(face_box)
   img.thumbnail(maxsize)
   if first:  # tkinter is inactive the first time
     bio = io.BytesIO()
@@ -32,12 +34,10 @@ def get_img_data(f, maxsize=(600, 450), first=False):
 
 def get_face_position(img_path):
   img = cv2.imread(img_path)
-  print(img_path)
-#     plt.imshow(img, cmap='gray')
   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  # face_box = face_cascade.detectMultiScale(img_gray)
-  face_box = face_cascade.detectMultiScale(img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(150, 150))
-
+  face_box = face_cascade.detectMultiScale(img_gray)
+  # face_box = face_cascade.detectMultiScale(img_gray, scaleFactor=1.05, minNeighbors=3, minSize=(120, 120))
+  # print(face_box)
   for i in range(face_box.shape[0]):
     face_box[i][2] += face_box[i][0]
     face_box[i][3] += face_box[i][1]
@@ -47,27 +47,51 @@ def get_face_position(img_path):
 for img_path in sys.argv:
   if img_path == 'test.py':
     continue
-  box_list.append(img_path)
-  get_face_position(img_path)
+  path_list.append(img_path)
 
 
-for box in box_list:
-  img_list.append(sg.Image(data=get_img_data(box, first=True)))
+for path in path_list:
+  face_box = get_face_position(path)
+  print(face_box)
+  for i in range(face_box.shape[0]):
+    img_list.append(sg.Image(data=get_img_data(path, face_box[i], first=True)))
 
 
-# layout = [col_read_file, img_list]
-layout = [[sg.Text('Please enter a number.'), sg.InputText()],
-          [sg.Button('OK'), sg.Button('Cancel')],
-          img_list]
+if len(img_list) > 1:
+  candidate_layout = [[sg.Text('Please enter a number between 1 and ' + str(face_box.shape[0]) + '.'), sg.InputText()],
+                      [sg.Button('OK'), sg.Button('Cancel')],
+                      img_list]
+else:
+  candidate_layout = [[sg.Text('Is this the face?')],
+                      [sg.Button('Yes'), sg.Button('No')],
+                      img_list]
 
-window = sg.Window('Choose face', layout, return_keyboard_events=True, use_default_focus=False)
 
+candidate_window = sg.Window('Choose the face', candidate_layout, return_keyboard_events=True, use_default_focus=False)
 # loop reading the user input and displaying image, filename
 while True:
-  event, values = window.read()
-  if event == sg.WIN_CLOSED or event == 'Cancel':
+  event, values = candidate_window.read()
+  if event == sg.WIN_CLOSED or event == 'Cancel' or event == 'No':
     break
-  elif event == 'OK':
+  elif event == 'OK' or event == 'Yes':
     print('What you entered：', values[0])  # emotions[int(values[0])]
 
-window.close()
+candidate_window.close()
+
+
+result_layout = [[sg.Text('ここは1行目')],
+                 [sg.Text('ここは2行目：適当に文字を入力してください'), sg.InputText()],
+                 [sg.Button('OK'), sg.Button('キャンセル')]]
+
+# ウィンドウの生成
+result_window = sg.Window('サンプルプログラム', result_layout)
+
+# イベントループ
+while True:
+  event, values = result_window.read()
+  if event == sg.WIN_CLOSED or event == 'キャンセル':
+    break
+  elif event == 'OK':
+    print('あなたが入力した値： ', values[0])  # emotions[int(values[0])]
+
+result_window.close()
